8d681b2bf4b440952876808b0a6a7924744d127e
Keerthna Murali <keerthna.murali@kws-MacBook-Pro-156.local>
2020-01-09 14:52:27 -0600

    new changes

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 echo "Copying build to Storage"
 gsutil defacl ch -u AllUsers:READER gs://$STORAGE_BUCKET
 gsutil rsync -R /tmp/workspace/code gs://$STORAGE_BUCKET
-# gsutil setmeta -h "Cache-Control:private, max-age=0, no-transform" \
-#   gs://$STORAGE_BUCKET/*.js
+gsutil setmeta -h "Cache-Control:private, max-age=0, no-transform" \
+  gs://$STORAGE_BUCKET/*.js
